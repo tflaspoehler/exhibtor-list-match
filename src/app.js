1498823c249f1795d3f5cc1bf3c0594398423fa2
Tim Flaspoehler <tflaspoehler@tims-mbp.americasmart.com>
2019-06-27 16:17:30 -0400

    too many issues with the list but this is semi-finalized

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-directoryApp = angular.module("directoryApp", ['ngRoute'])
+exhibitorMatchApp = angular.module("exhibitorMatchApp", ['ngRoute'])
 
 //-----------------------------------------------------------------------//
 // change scope templating string to insert angular stuff from ${} to    //
@@ -24,16 +24,98 @@ directoryApp = angular.module("directoryApp", ['ngRoute'])
 	};
 }])
 //----------------------------------//
-
 //-----------------------------------------------------------------------//
 //      controller to sends AJAX request and returns it to the view      //
 //-----------------------------------------------------------------------//
-.controller("directoryController", ['$scope', 'getRequest', function directoryController($scope, getRequest) {
+.controller("exhibitorMatchController", ['$scope', 'getRequest', function directoryController($scope, getRequest) {
 
 	var vm = this;
 
+	var outputExhibitor = function(exhibitor) {
+		return '<li><a href="' + exhibitor.link + '"><p>' + exhibitor.showroomName + '</p></a></li>\\n';
+	}
+
+	vm.matchExhibitors = function(keywords) {
+		console.log(vm.exhibitors);
+		vm.matchedExhibitors = [];
+		vm.unmatchedExhibitors = [];
+		vm.allExhibitors = [];
+		vm.outputText = '';
+
+		keywords = keywords.split('\n').map(function(keyword) {
+			keyword = keyword.replace('Blus ', 'Blush ')
+							 .replace('APlus', 'A Plus')
+							 .replace(/[\u2018\u2019]/g, "'")
+							 .replace(/[\u201C\u201D]/g, '"')
+							 .replace('Desing', 'Design')
+							 .replace(' by Alexia','')
+							 .trim();
+			return keyword;
+		}).join('\n');
+
+		keywords.split('\n').forEach(function(keyword) {
+			var guess = vm.exhibitors.find(x => x.showroomName.replace('and', '&').toLowerCase() == keyword.trim().toLowerCase());
+
+			if (!guess) {
+				guess = vm.exhibitors.find(x => x.showroomName.trim().toLowerCase().replace('and', '&').includes(keyword.trim().toLowerCase()));
+			}
+
+			if (!guess) {
+				var possibilities = []
+				var found = false;
+				vm.exhibitors.forEach(function(exhibitor) {
+					if (exhibitor.showroomName.toLowerCase()
+											  .replace(/[\u2018\u2019]/g, "'")
+  											  .replace(/[\u201C\u201D]/g, '"').includes(keyword.trim()
+  											  												   .split()[0]
+  											  												   .toLowerCase()
+  											  												   .replace(/[\u2018\u2019]/g, "'")
+  											  												   .replace(/[\u201C\u201D]/g, '"'))) {
+						possibilities.push(exhibitor);
+						found = true;
+					}
+				});
+				if (!found) {
+					console.log('>>>>> did not find ' + keyword);
+					vm.unmatchedExhibitors.push({keyword: keyword});
+					vm.outputText += outputExhibitor({showroomName: 'FIX: ' + keyword, exhibitorID: 0});
+					vm.allExhibitors.push({keyword: keyword, showroomName: keyword, link: "https://www.americasmart.com/browse/#/search?q=" + keyword.trim()[0].split()[0].toLowerCase()});
+				}
+				else {
+					if (possibilities.length > 1) {
+						console.log('positibilities for ' + showroomName + ' ' + possibilities);
+					}
+					else {
+						guess = possibilities[0];
+						possibilities[0].keyword = keyword;
+						possibilities[0].link = 'https://www.americasmart.com/browse/#/exhibitor/' + possibilities[0].exhibitorID;
+						vm.outputText += outputExhibitor(possibilities[0]);
+						vm.allExhibitors.push(possibilities[0]);
+					}
+				}
+			}
+			else {
+				guess.keyword = keyword;
+				guess.link = 'https://www.americasmart.com/browse/#/exhibitor/' + guess.exhibitorID;
+				vm.matchedExhibitors.push(guess);
+				vm.allExhibitors.push(guess);
+				vm.outputText += outputExhibitor(guess);
+			}
+		});
+		vm.outputText = vm.allExhibitors.sort(function(a, b) {
+			if (a.showroomName > b.showroomName) { return 1; }
+			if (a.showroomName < b.showroomName) { return -1; }
+			return 0;
+		}).map(function(exhibitor) {
+			return outputExhibitor(exhibitor);
+		}).join('');
+	}
+
+	var list = ""
+
 	// get employee information from WEM database
-	getRequest.getData("https://wem.americasmart.com/api/EmployeeDirectory?byDep").then(function(employees) {
+	getRequest.getData("https://wem.americasmart.com/api/search/exhibitor").then(function(exhibitors) {
+		vm.exhibitors = exhibitors;
 	});
 }]);
 //-----------------------------------------------------------------------//
